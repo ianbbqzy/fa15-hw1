dc0c39d08de4ecbcf9c8a660f689f10eedc070b1
Ian Lee <ianlee@airbears2-10-142-48-170.airbears2.1918.berkeley.edu>
2016-02-11 13:18:58 -0800

    done

--- foobar.rb
+++ foobar.rb
@@ -1,3 +1,9 @@
 class Foobar
   # Q4 CODE HERE
+  def self.baz(a)
+    a = a.collect { |x| x.to_i + 2 }
+    a.keep_if { |x| x < 10 }
+    a.keep_if { |x| x % 2 == 0 }
+    a.inject(0) {|sum, i|  sum + i }
+  end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,13 +1,15 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
+  (a + b)**2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
+  # a.sort { |x,y|  x <>}
+  a = a.sort
+  a.collect { |x| x + 1 }
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
